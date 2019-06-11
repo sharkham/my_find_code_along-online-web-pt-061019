0e7d106f9eff52dff4e3a53ea818d300ceb22c52
Sam Markham <sam.e.markham@gmail.com>
2019-06-11 01:20:09 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,11 +1,19 @@
 require 'pry'
 
+# def my_find(collection)
+#   i = 0 
+#   while i < collection.length 
+#     if yield(collection[i])
+#       return collection[i]
+#     end 
+#     i += 1 
+#   end 
+# end
+
 def my_find(collection)
   i = 0 
   while i < collection.length 
-    if yield(collection[i])
-      return collection[i]
-    end 
+    return collection[i] if yield(collection[i])
     i += 1 
   end 
-end
+end 
